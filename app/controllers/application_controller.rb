c6613f371399be76e9745794b73175515d19e93a
Varsion <jianhual12@outlook.com>
2022-07-17 15:27:59 +0800

    feat: add current function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  def current_user
+    token = request.headers["Authorization"]
+    begin
+      data = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+      user = User.find_by(id: data["user_id"])
+
+      {user: user, token: token}
+    rescue JWT::DecodeError
+      {}
+    end
+  end
 end
